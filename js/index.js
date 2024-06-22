b29bac5d4f7093ee3e9d0fbd9e45827b19262255
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-06-23 00:01:40 +0500

    master

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// ==================== 1- misol =================
+
+// const students = [
+//     { ism: "Ali", yosh: 20, baholari: [90, 85, 88] },
+//     { ism: "Vali", yosh: 21, baholari: [85, 87, 90] },
+//     { ism: "Sami", yosh: 22, baholari: [88, 92, 84] }
+// ];
+
+// function result(students) {
+//     let bestStudent = null;
+//     let highestAverage = 0;
+
+//     students.forEach(student => {
+//         const averageGrade = student.baholari.reduce((sum, grade) => sum + grade, 0) / student.baholari.length;
+        
+//         if (averageGrade > highestAverage) {
+//             highestAverage = averageGrade;
+//             bestStudent = student.ism;
+//         }
+//     });
+
+//     return bestStudent;
+// }
+
+// console.log(result(students));
+
+
+
+
+// ==================== 2- misol =================
+
